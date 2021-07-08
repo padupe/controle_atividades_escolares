ed17a691eea2263b7b4c520e09611f4205bb3fa1
Paulo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-07-08 16:20:02 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  `Server Started on port ${process.env.PORT} with env ${process.env.NODE_ENV}! 🚀`;
+});
+
+app.use('/', routes);
+
+module.exports = app;
